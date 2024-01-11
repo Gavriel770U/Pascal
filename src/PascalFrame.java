3146aa17e463461b465e0307e5a1a32628046fcb
Gavriel770U <kraserultra@gmail.com>
2024-01-11 14:55:44 +0200

    Add title

--- src/PascalFrame.java
+++ src/PascalFrame.java
@@ -4,12 +4,12 @@ public class PascalFrame extends JFrame
 {
     public PascalFrame()
     {
+        setTitle("Gavriel's Pascal Triangles GUI");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(Settings.WIDTH.ivalue, Settings.HEIGHT.ivalue);
         setVisible(true);
         add(new PascalPanel());
         setLayout(null);
         setResizable(false);
-        setBounds(0, 0, Settings.WIDTH.ivalue, Settings.HEIGHT.ivalue);
     }
 }
