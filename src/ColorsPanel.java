1d70a0403a29005d6065680e156ef530b95fd975
Gavriel770U <kraserultra@gmail.com>
2024-02-21 23:13:11 +0200

    Update setup little bit

--- src/ColorsPanel.java
+++ src/ColorsPanel.java
@@ -4,26 +4,46 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JColorChooser;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 
-public class ColorsPanel extends JPanel implements ActionListener
+public class ColorsPanel extends JPanel implements ActionListener, ItemListener
 {
+    private JComboBox<String> modComboBox;
+
     public ColorsPanel()
     {   
         setPreferredSize(new Dimension(Settings.SETTINGS_WIDTH.ivalue, Settings.SETTINGS_HEIGHT.ivalue));
         setDoubleBuffered(true);
         setBackground(Color.BLACK);
         setFocusable(true);
-        setLayout(new GridLayout(2, 2));
+        setLayout(getLayout());
         requestFocusInWindow();
 
         JButton button = new JButton("Pad Color");
-        button.setBounds(0, 0, 100, 30);
+        button.setPreferredSize(new Dimension(100, 30));
         button.addActionListener(this);
-        
-        add(new JLabel("TODO: Mod value to change its color JCombobox"));       
+
+        int modValuesAmount = Settings.MOD_SLIDER_MAX_MOD.ivalue - Settings.MOD_SLIDER_MIN_MOD.ivalue + 1;
+        String[] modValuesStrings = new String[modValuesAmount];
+
+        for (int i = 0; i < modValuesAmount; i++)
+        {
+            modValuesStrings[i] = Integer.toString(i + Settings.MOD_SLIDER_MIN_MOD.ivalue);
+        }
+         
+        modComboBox = new JComboBox<>(modValuesStrings);
+        modComboBox.setSelectedIndex(Settings.MOD_SLIDER_INIT_MOD.ivalue);
+        modComboBox.addItemListener(this);
+        modComboBox.setEditable(false);
+        modComboBox.setPreferredSize(new Dimension(50, 20));
+
+        add(modComboBox);       
         add(new JLabel("MOD VALUE | RGB VALUES"));
         add(button);
         add(new JLabel("TODO: Add Horizontal slider for RGB values choosing and viewing"));
@@ -33,5 +53,13 @@ public class ColorsPanel extends JPanel implements ActionListener
     {  
         Color c = JColorChooser.showDialog(this,"Choose",Color.CYAN);  
         System.out.println(c);
+    }
+
+    public void itemStateChanged(ItemEvent e)
+    {
+        if (e.getSource() == this.modComboBox)
+        {
+            System.out.println(this.modComboBox.getSelectedItem());
+        }
     }
 }
